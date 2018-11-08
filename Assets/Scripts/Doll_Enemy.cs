5b69ac5b37ca5f28a348e7355bc6b516d8255347
arkadiusz_slowikowski_discovr <arkadiusz.slowikowski@discovr.pl>
2018-11-08 08:04:31 +0100

    Done

---
+++ Assets/Scripts/Doll_Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Doll_Enemy : Enemy
+{
+	public override void Die()
+	{
+		Collider[] bodyColliders = GetComponentsInChildren<Collider>();
+
+		foreach (Collider col in bodyColliders)
+		{
+			Rigidbody rb = col.GetComponent<Rigidbody>();
+			rb.isKinematic = false;
+			Destroy(rb, 3);
+			StartCoroutine(TurnOffCollider(col, 3.2f));
+		}
+	}
+
+	private IEnumerator TurnOffCollider(Collider collider, float delay)
+	{
+		yield return new WaitForSeconds(delay);
+
+		Destroy(collider);
+	}
+}
